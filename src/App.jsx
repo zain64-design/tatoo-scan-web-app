5e4f745a4a3086f33397217bb0216a32ab00a071
zain64-design <zainyzain64@gmail.com>
2024-10-23 04:45:25 +0500

    hashrouter added vercel.json removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import Home from './pages/Home';
 // import About from './pages/About';
