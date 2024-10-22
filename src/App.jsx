2ec3cd59be22f0e4d2418cfa2c0d058dfd2814a1
zain64-design <zainyzain64@gmail.com>
2024-10-23 04:14:02 +0500

    lazy load and suspense is add on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Blogs from './pages/Blogs';
-import Contact from './pages/Contact';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Blogs from './pages/Blogs';
+// import Contact from './pages/Contact';
+// import Layout from './components/Layout/Layout';
+// import BlogDetails from './pages/BlogDetails';
 import Layout from './components/Layout/Layout';
-import BlogDetails from './pages/BlogDetails';
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Services = lazy(() => import('./pages/Services'));
+const Blogs = lazy(() => import('./pages/Blogs'));
+const BlogDetails = lazy(() => import('./pages/BlogDetails'));
+const Contact = lazy(() => import('./pages/Contact'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      suspense: false,
+      suspense: true,
       staleTime: 5000,       
       cacheTime: 120000,      // Cache time is now 2 minutes (120,000 ms) 
       retry: 2,              // Retry the request up to 2 times
@@ -28,14 +35,16 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Layout>
-        <Routes>
-          <Route exact path="/" index element={<Home/>} />
-          <Route exact path="/about-us" element={<About/>} />
-          <Route exact path="/services" element={<Services/>} />
-          <Route exact path="/blogs" element={<Blogs/>} />
-          <Route exact path="/blog-detail/:id" element={<BlogDetails/>} />
-          <Route exact path="/contact" element={<Contact/>} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about-us" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blog-detail/:id" element={<BlogDetails />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Suspense>
         </Layout>
       </Router>
       </QueryClientProvider>
