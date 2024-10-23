442185d13dab3e8168a7cc47dcd5eb53ec6cb92d
zain64-design <zainyzain64@gmail.com>
2024-10-23 05:16:15 +0500

    route loader removed from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,11 @@ import SecHomeAbout from '../components/About/SecHomeAbout'
 import CTABannerOne from '../components/HomeBanner/CTABannerOne'
 import SecHomeGallery from '../components/Gallery/SecHomeGallery'
 import CTABannerThree from '../components/HomeBanner/CTABannerThree'
-import RouteLoader from '../components/Loader/RouteLoader'
 
 const Home = () => {
   return (
     <>
     <SecHomeBanner/>
-    <RouteLoader/>
     <SecHomeAbout/>
     <CTABannerOne/>
     <SecHomeGallery/>
